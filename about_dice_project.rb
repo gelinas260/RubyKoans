34f63b77fb32dcc37a1f230562b54a0d532e44c2
Timothy Gelinas <tgelinas@mreach.com>
2017-08-03 08:49:23 -0400

    COMPLETED: About inheritance

--- about_dice_project.rb
+++ about_dice_project.rb
@@ -49,13 +49,9 @@ class AboutDiceProject < Neo::Koan
 
     dice.roll(5)
     first_time = dice.values
-    puts first_time
-
-    puts "-------"
 
     dice.roll(5)
     second_time = dice.values
-    puts second_time
 
     assert_not_equal first_time, second_time,
       "Two rolls should not be equal"
